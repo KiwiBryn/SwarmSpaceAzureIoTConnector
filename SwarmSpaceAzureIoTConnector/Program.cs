83dc348bd169c277b840822af9d58d4b7bdbf062
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-01-26 13:59:40 +1300

    Initial version og Uplink queue trigger

--- SwarmSpaceAzureIoTConnector/Program.cs
+++ SwarmSpaceAzureIoTConnector/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace SwarmSpaceAzureIoTConnector
+﻿// Copyright (c) January 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devmobile.IoT.SwarmSpaceAzureIoTConnector.Connector
 {
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var host = new HostBuilder()
+               .ConfigureFunctionsWorkerDefaults()
+               .ConfigureAppConfiguration(c =>
+               {
+#if DEBUG
+                   c.AddUserSecrets("00f193f0-7cd4-43c8-a885-336029a808b9");
+#endif
+                   c.AddEnvironmentVariables();
+               })
+                .ConfigureLogging((context, l) =>
+                {
+                    l.AddConsole();
+                    l.AddApplicationInsightsWebJobs(o => o.ConnectionString = (context.Configuration.GetConnectionString("ApplicationInsights")));
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
         }
     }
 }
---
+++ SwarmSpaceAzureIoTConnector/UplinkQueueTrigger.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) January 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devmobile.IoT.SwarmSpaceAzureIoTConnector.Connector
+{
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Extensions.Logging;
+
+    public class UplinkQueueTriggerProcessor
+    {
+        private readonly ILogger _logger;
+
+        public UplinkQueueTriggerProcessor(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<UplinkQueueTriggerProcessor>();
+        }
+
+        [Function("UplinkQueueTrigger")]
+        public async Task Run([QueueTrigger("uplink1", Connection = "AzureFunctionsStorage")] string myQueueItem)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
