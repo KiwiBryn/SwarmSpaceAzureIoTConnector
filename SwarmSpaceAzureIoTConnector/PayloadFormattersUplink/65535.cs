2b91f6762e9149bb0c89a639b41a53acc412c693
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-01-30 14:14:24 +1300

    Added mappings for other data sent by EvalKit

--- SwarmSpaceAzureIoTConnector/PayloadFormattersUplink/65535.cs
+++ SwarmSpaceAzureIoTConnector/PayloadFormattersUplink/65535.cs
@@ -22,6 +22,23 @@ public class FormatterUplink : PayloadFormatter.IFormatterUplink
             telemetryEvent.Add("DeviceLocation", location);
         }
 
+        // Course & speed
+        telemetryEvent.Add("Course", payloadJson.GetValue("c"));
+        telemetryEvent.Add("Speed", payloadJson.GetValue("s"));
+
+        // Battery voltage & current
+        telemetryEvent.Add("BatteryVoltage", payloadJson.GetValue("bv"));
+        telemetryEvent.Add("BatteryCurrent", payloadJson.GetValue("bi"));
+
+        // Solar voltage
+        telemetryEvent.Add("SolarVoltage", payloadJson.GetValue("sv"));
+
+        // Modem current 
+        telemetryEvent.Add("ModemCurrent", payloadJson.GetValue("ti"));
+
+        // RSSI
+        telemetryEvent.Add("RSSI", payloadJson.GetValue("r"));
+
         Message ioTHubmessage = new Message(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(telemetryEvent)));
 
         ioTHubmessage.Properties.Add("iothub-creation-time-utc", DateTimeOffset.FromUnixTimeSeconds((long)payloadJson.GetValue("d")).ToString("s", CultureInfo.InvariantCulture));
