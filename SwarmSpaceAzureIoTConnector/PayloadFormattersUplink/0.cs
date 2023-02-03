2e1cebadaa90f74bc76f03ad7d17a4a102f12a80
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-02-03 14:03:37 +1300

    Default formatter now extracts/displays bytes and ASCII representations of payload

--- SwarmSpaceAzureIoTConnector/PayloadFormattersUplink/0.cs
+++ SwarmSpaceAzureIoTConnector/PayloadFormattersUplink/0.cs
@@ -8,5 +8,7 @@ public class FormatterUplink : PayloadFormatter.IFormatterUplink
 {
     public void Evaluate(Dictionary<string, string> properties, uint organisationId, uint deviceId, byte deviceType, ushort userApplicationId, JObject telemetryEvent, JObject payloadJson, string payloadText, byte[] payloadBytes)
     {
-     }
+        telemetryEvent.Add("ASCII", payloadText);
+        telemetryEvent.Add("Bits", BitConverter.ToString(payloadBytes,2));
+    }
 }
