497e723f0346fe4bf6ebcabfed106405f30e7373
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-02-04 19:47:13 +1300

    Fixed up index so the BitConvertor was starting at beginning not 2 bytyes in

--- SwarmSpaceAzureIoTConnector/PayloadFormattersUplink/0.cs
+++ SwarmSpaceAzureIoTConnector/PayloadFormattersUplink/0.cs
@@ -9,6 +9,6 @@ public class FormatterUplink : PayloadFormatter.IFormatterUplink
     public void Evaluate(Dictionary<string, string> properties, uint organisationId, uint deviceId, byte deviceType, ushort userApplicationId, JObject telemetryEvent, JObject payloadJson, string payloadText, byte[] payloadBytes)
     {
         telemetryEvent.Add("ASCII", payloadText);
-        telemetryEvent.Add("Bits", BitConverter.ToString(payloadBytes,2));
+        telemetryEvent.Add("Bits", BitConverter.ToString(payloadBytes));
     }
 }
