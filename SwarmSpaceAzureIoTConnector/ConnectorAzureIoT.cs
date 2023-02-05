53e07679c01262b708854235e887c45b4049de46
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-02-05 13:05:25 +1300

    Moved Azure IoT Hub DPS code into "shared" file as used by bother Azure IoT Central and Azure IoT Hub connectivity.

--- SwarmSpaceAzureIoTConnector/ConnectorAzureIoT.cs
+++ SwarmSpaceAzureIoTConnector/ConnectorAzureIoT.cs
@@ -15,14 +15,89 @@
 //---------------------------------------------------------------------------------
 namespace devMobile.IoT.SwarmSpaceAzureIoTConnector.Connector
 {
+    using System.Security.Cryptography;
+    using System;
     using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Provisioning.Client.PlugAndPlay;
+    using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+    using Microsoft.Azure.Devices.Provisioning.Client;
+    using Microsoft.Azure.Devices.Shared;
     using Microsoft.Extensions.Logging;
 
     partial class AzureDeviceClientCache : IAzureDeviceClientCache
     {
+        private async Task<DeviceClient> AzureIoTHubDeviceProvisioningServiceConnectAsync(string deviceId, object context, Models.AzureDeviceProvisioningService deviceProvisioningService)
+        {
+            DeviceClient deviceClient;
+
+            string deviceKey;
+            using (var hmac = new HMACSHA256(Convert.FromBase64String(deviceProvisioningService.GroupEnrollmentKey)))
+            {
+                deviceKey = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(deviceId)));
+            }
+
+            using (var securityProvider = new SecurityProviderSymmetricKey(deviceId, deviceKey, null))
+            {
+                using (var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly))
+                {
+                    DeviceRegistrationResult result;
+
+                    ProvisioningDeviceClient provClient = ProvisioningDeviceClient.Create(
+                        deviceProvisioningService.GlobalDeviceEndpoint,
+                        deviceProvisioningService.IdScope,
+                        securityProvider,
+                        transport);
+
+                    if (!string.IsNullOrEmpty(deviceProvisioningService.DtdlModelId))
+                    {
+                        ProvisioningRegistrationAdditionalData provisioningRegistrationAdditionalData = new ProvisioningRegistrationAdditionalData()
+                        {
+                            JsonData = PnpConvention.CreateDpsPayload(deviceProvisioningService.DtdlModelId)
+                        };
+                        result = await provClient.RegisterAsync(provisioningRegistrationAdditionalData);
+                    }
+                    else
+                    {
+                        result = await provClient.RegisterAsync();
+                    }
+
+                    if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+                    {
+                        _logger.LogWarning("Uplink-DeviceID:{deviceId} RegisterAsync status:{result.Status} failed ", deviceId, result.Status);
+
+                        throw new ApplicationException($"Uplink-DeviceID:{deviceId} RegisterAsync status:{result.Status} failed");
+                    }
+
+                    IAuthenticationMethod authentication = new DeviceAuthenticationWithRegistrySymmetricKey(result.DeviceId, (securityProvider as SecurityProviderSymmetricKey).GetPrimaryKey());
+
+                    deviceClient = DeviceClient.Create(result.AssignedHub, authentication, TransportSettings);
+                }
+            }
+
+            switch (_azureIoTSettings.ApplicationType)
+            {
+                case Models.ApplicationType.AzureIotHub:
+                    await deviceClient.SetReceiveMessageHandlerAsync(AzureIoTHubMessageHandler, context);
+                    break;
+                case Models.ApplicationType.AzureIoTCentral:
+                    await deviceClient.SetReceiveMessageHandlerAsync(AzureIoTCentralMessageHandler, context);
+                    break;
+                default:
+                    _logger.LogError("Azure IoT Hub ApplicationType unknown {0}", _azureIoTSettings.AzureIotHub.ConnectionType);
+
+                    throw new NotImplementedException("AzureIoT Hub unsupported ApplicationType");
+            }
+
+            await deviceClient.SetMethodDefaultHandlerAsync(AzureIoTHubClientDefaultMethodHandler, context);
+
+            await deviceClient.OpenAsync();
+
+            return deviceClient;
+        }
+
         private async Task<MethodResponse> AzureIoTHubClientDefaultMethodHandler(MethodRequest methodRequest, object userContext)
         {
             Models.AzureIoTDeviceClientContext context = (Models.AzureIoTDeviceClientContext)userContext;
