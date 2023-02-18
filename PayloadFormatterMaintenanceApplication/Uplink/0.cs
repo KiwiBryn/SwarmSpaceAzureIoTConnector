73370c1224bb125af11f3b2dbe3c5f97c3133846
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-02-18 16:39:51 +1300

    Sample uplink transformers

---
+++ PayloadFormatterMaintenanceApplication/Uplink/0.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FormatterUplink : PayloadFormatter.IFormatterUplink
+{
+    public void Evaluate(Dictionary<string, string> properties, uint organisationId, uint deviceId, byte deviceType, ushort userApplicationId, JObject telemetryEvent, JObject payloadJson, string payloadText, byte[] payloadBytes)
+    {
+        telemetryEvent.Add("ASCII", payloadText);
+        telemetryEvent.Add("Bits", BitConverter.ToString(payloadBytes));
+    }
+}
---
+++ PayloadFormatterMaintenanceApplication/Uplink/65535.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+
+public class FormatterUplink : PayloadFormatter.IFormatterUplink
+{
+    public void Evaluate(Dictionary<string, string> properties, uint organisationId, uint deviceId, byte deviceType, ushort userApplicationId, JObject telemetryEvent, JObject payloadJson, string payloadText, byte[] payloadBytes)
+    {
+        if ((payloadText != "") && (payloadJson != null))
+        {
+            JObject location = new JObject();
+
+            location.Add("lat", payloadJson.GetValue("lt"));
+            location.Add("lon", payloadJson.GetValue("ln"));
+            location.Add("alt", payloadJson.GetValue("a"));
+
+            telemetryEvent.Add("DeviceLocation", location);
+        }
+
+        // Course & speed
+        telemetryEvent.Add("Course", payloadJson.GetValue("c"));
+        telemetryEvent.Add("Speed", payloadJson.GetValue("s"));
+
+        // Battery voltage & current
+        telemetryEvent.Add("BatteryVoltage", payloadJson.GetValue("bv"));
+        telemetryEvent.Add("BatteryCurrent", payloadJson.GetValue("bi"));
+
+        // Solar voltage
+        telemetryEvent.Add("SolarVoltage", payloadJson.GetValue("sv"));
+
+        // Modem current 
+        telemetryEvent.Add("ModemCurrent", payloadJson.GetValue("ti"));
+
+        // RSSI
+        telemetryEvent.Add("RSSI", payloadJson.GetValue("r"));
+
+        properties.Add("iothub-creation-time-utc", DateTimeOffset.FromUnixTimeSeconds((long)payloadJson.GetValue("d")).ToString("s", CultureInfo.InvariantCulture));
+    }
+}
